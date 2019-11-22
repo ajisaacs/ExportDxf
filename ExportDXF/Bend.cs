222431190b9e0dd43fe2a5c505c745889ea6b486
AJ <ajisaacs@rogersengineering.net>
2019-11-22 06:25:38 -0500

    Added comments for bend class.

--- ExportDXF/Bend.cs
+++ ExportDXF/Bend.cs
@@ -2,13 +2,20 @@
 {
     internal class Bend
     {
-        public BendDirection Direction { get; set; }
-
         public double BendLineAngle { get; set; }
+
+        public BendDirection Direction { get; set; }
 
         public double Angle { get; set; }
 
+        /// <summary>
+        /// X coordinate of the bend line in meters
+        /// </summary>
         public double X { get; set; }
+
+        /// <summary>
+        /// Y coordinate of the bend line in meters
+        /// </summary>
         public double Y { get; set; }
     }
 }
